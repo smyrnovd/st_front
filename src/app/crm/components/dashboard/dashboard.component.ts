51e5e4495ca59c69afebf76bd7a67a5889268033
ellan77 <smyrnovd@gmail.com>
2023-09-06 15:13:57 +0300

    grid 2

--- src/app/crm/components/dashboard/dashboard.component.ts
+++ src/app/crm/components/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
     templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit, OnDestroy {
     items!: MenuItem[];
@@ -15,6 +16,45 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
     subscription!: Subscription;
     date: string;
+
+    infoBlocks = [
+        {
+            title: 'Revenue',
+            value: '$2.100',
+            color_bg: 'bg-orange-100',
+            icon: 'pi pi-map-marker',
+            color: 'text-orange-500',
+            small_num: '%12',
+            small_text: 'since last week',
+        },
+        {
+            title: 'Customers',
+            value: '28441',
+            color_bg: 'bg-cyan-100',
+            icon: 'pi pi-inbox',
+            color: 'text-cyan-500',
+            small_num: '520',
+            small_text: 'newly registered',
+        },
+        {
+            title: 'Comments',
+            value: '152 Unread',
+            color_bg: 'bg-purple-100',
+            icon: 'pi pi-comment',
+            color: 'text-purple-500',
+            small_num: '85',
+            small_text: 'responded',
+        },
+        {
+            title: 'Orders',
+            value: '44',
+            color_bg: 'bg-blue-100',
+            icon: 'pi pi-shopping-cart',
+            color: 'text-blue-500',
+            small_num: '24 new',
+            small_text: 'since last visit',
+        },
+    ];
 
     constructor(
         private productService: ProductService,
